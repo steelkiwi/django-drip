8d1f2f351d12194d743b97c4f4a3d2aabf373d68
Alexey Milogradov <milogradov@steelkiwi.com>
2016-05-12 18:37:19 +0300

    PHB-329: Fix name shadowing in querysets

--- drip/querysets.py
+++ drip/querysets.py
@@ -5,5 +5,5 @@ class DripQueryset(QuerySet):
 
     def send(self, use_mailgun=True):
         for drip in self:
-            drip = drip.drip_mailgun if use_mailgun else drip.drip
-            drip.run()
+            drip_ = drip.drip_mailgun if use_mailgun else drip.drip
+            drip_.run()
