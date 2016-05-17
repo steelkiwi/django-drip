6524937f8e811fed3d97b672ca44a3bb2006e05e
Alexey Milogradov <milogradov@steelkiwi.com>
2016-05-17 18:56:22 +0300

    PHB-351: Add blog_entry to context

--- drip/drips.py
+++ drip/drips.py
@@ -288,6 +288,7 @@ class MailgunBatchMessage(DripMessage):
         f = self.drip_base.MAILGUN_VARIABLE_GENERATION_FUNCTION or self.map_variable
         if not self._context:
             self._context = Context({'user': {v: f(v) for v in self.drip_base.variables}})
+            self._context['blog_entry'] = self.drip_base.drip_instance.get_blog_entries_for_newsletter()
         return self._context
 
     def mailgun_variables_for_user(self, user, strict=True):
@@ -360,6 +361,7 @@ class DripMailgun(DripBase):
         self.tags_list = kwargs.pop('tags_list', [])
         self.template_base = kwargs.pop('template_base')
         self.base_template_html_path = kwargs.pop('base_template_html_path')
+        self.drip_instance = kwargs.pop('drip_instance')
         super(DripMailgun, self).__init__(*args, **kwargs)
 
         self.MAILGUN_VARIABLE_GENERATION_FUNCTION =\
